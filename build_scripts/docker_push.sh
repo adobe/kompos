8dc4cf730634144ae0e60043300c7de51e81c400
Daniel Coman <daniel.alex.coman@gmail.com>
2020-01-20 16:40:21 +0200

    [RELEASE] - Release version 0.2.8

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag kompos adobe/kompos:0.2.7
-docker push adobe/kompos:0.2.7
+docker tag kompos adobe/kompos:0.2.8
+docker push adobe/kompos:0.2.8
--- src/kompos/__init__.py
+++ src/kompos/__init__.py
@@ -14,7 +14,7 @@ from subprocess import call, Popen, PIPE
 from .cli import display
 
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 
 
 class Executor():
