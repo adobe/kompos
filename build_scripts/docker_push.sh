e710addb3e286031a8b3f0ab54037d0ac645d733
Daniel Coman <daniel.alex.coman@gmail.com>
2020-05-07 17:41:37 +0300

    [RELEASE] - Release version 0.2.19

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag kompos adobe/kompos:0.2.18
-docker push adobe/kompos:0.2.18
+docker tag kompos adobe/kompos:0.2.19
+docker push adobe/kompos:0.2.19
