d56968d1d7af9c83df9b033118acb9396618d057
Daniel Coman <daniel.alex.coman@gmail.com>
2023-04-27 14:20:56 +0300

    [RELEASE] - Release version 0.4.3

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag kompos adobe/kompos:0.4.2
-docker push adobe/kompos:0.4.2
+docker tag kompos adobe/kompos:0.4.3
+docker push adobe/kompos:0.4.3
--- src/kompos/__init__.py
+++ src/kompos/__init__.py
@@ -12,7 +12,7 @@ from subprocess import call
 
 from termcolor import colored
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 
 def display(msg, color):
